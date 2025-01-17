1fb9007ab430ecec15ba6d993685bc330286eeda
Dhimahi Trupti Mehta <dhimahimehta01@gmail.com>
2025-01-17 17:49:47 +0530

    Added elif condition

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,6 +1,8 @@
-def encrypt(text, shift)
+def encrypt(text, shift):
   encrypted_text=""
-  for i in range(len(text))
+  for i in range(len(text)):
     char=text[i]
-    if(char.isupper())
+    if(char.isupper()):
+      encrypted_text+=chr((ord(char)+shift-65)%26+65)
+    elif(char.islower()):
       encrypted_text+=chr((ord(char)+shift-65)%26+65)
